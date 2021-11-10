8d26ce276447474de7e5b961f086ca49e12aa18a
李凯 <li_kai@hisuntech.com>
2021-11-10 17:39:36 +0800

    add file

--- leetcode/primary/array_01/ch_09.go
+++ leetcode/primary/array_01/ch_09.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /**
 移动零
 
@@ -20,9 +22,43 @@ package main
 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
  */
 func moveZeroes(nums []int)  {
-	for i := 0; i<len(nums); i++ {}
+	left, right, n := 0, 0, len(nums)
+
+	for right < n {
+		if nums[right] != 0 {
+			nums[left], nums[right] = nums[right],nums[left]
+			left++
+		}
+		right ++
+	}
+	fmt.Println(nums)
 }
 
 func main() {
+	//moveZeroes([]int{0,1,0,3,12})
+	sums := []int{3, 2, 2, 3}
+	element := removeElement(sums, 3)
+	fmt.Println(element)
+	for i := 0; i < element; i++ {
+		fmt.Print(sums[i])
+	}
+}
+
+/**
+	拓展
+	给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+ */
+func removeElement(nums []int, val int) int {
+	j:=0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val{
+			nums[j] = nums[i]
+
+			j ++
+		}
+		nums[i] = 0
+	}
+	return j +1
 
 }
+
