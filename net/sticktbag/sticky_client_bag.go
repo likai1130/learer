e56b0f16ba149d5a9366b76ff43c8ed05c55d4b1
李凯 <li_kai@hisuntech.com>
2022-02-05 23:24:00 +0800

    tcp

---
+++ net/sticktbag/sticky_client_bag.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"learner/net/sticktbag/proto"
+	"net"
+)
+
+/**
+	粘包客户端代码
+ */
+/*func main() {
+	conn, err := net.Dial("tcp", "127.0.0.1:30000")
+	if err != nil {
+		fmt.Println("dial failed, err", err)
+		return
+	}
+	defer conn.Close()
+	for i := 0; i < 20; i++ {
+		msg := `Hello, Hello. How are you?`
+		conn.Write([]byte(msg))
+	}
+}
+*/
+
+/**
+	改造后
+ */
+
+func main() {
+	conn, err := net.Dial("tcp", "127.0.0.1:30000")
+	if err != nil {
+		fmt.Println("dial failed, err", err)
+		return
+	}
+	defer conn.Close()
+	for i := 0; i < 20; i++ {
+		msg := `Hello, Hello. How are you?`
+		data, err := proto.Encode(msg)
+		if err != nil {
+			fmt.Println("encode msg failed, err:", err)
+			return
+		}
+		conn.Write(data)
+	}
+}
